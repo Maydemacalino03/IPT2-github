952339121054f2ba0fa5ece38e40243df91ec087
Maydelyn <maydemacalino@gmail.com>
2024-09-10 20:47:11 +0700

    Add multiplication method

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,3 @@
-public class calculator {
-    public statuc void main (String [] args) {
-    int a = 5;
-    int b = 10;
-    int sum = a + b;
-    int sub = a -b;
-    int n1 = a * b;
-
-    System.out.println("Addition:" + sum);
-    System.out.println("Subtraction:" + sub);
-    System.out.println("multiplication:" + n1);
+public class Main {
+    
 }
-        }
-   }
